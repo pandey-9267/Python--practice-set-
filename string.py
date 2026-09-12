cd7225389ed86714fe15b43f323760a36d3844eb
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-13 01:55:06 +0530

    learn string how to find length

---
+++ string.py
@@ -0,0 +1,11 @@
+# WAP to input users first name and print its length
+
+# user_name = input("Enter user name: ")
+# print("User name length : ", len(user_name))
+
+# ---------------------------------------------------------
+
+# WAP to find the occurrence of '$' in a string
+
+str = input("Enter your string: ")
+print("No. of $ in this string: ", str.count("$"))
