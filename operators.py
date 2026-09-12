c84985911eaaa77eed96b1e9d7265aed923dad3f
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-13 01:41:23 +0530

    learn operators

---
+++ operators.py
@@ -0,0 +1,27 @@
+# WAP to take input from user and print their sum
+
+# num_1 = float(input("Enter your num_1 : "))
+# num_2 = float(input("Enter your num_2 : "))
+# sum = num_1 + num_2
+# print(sum)
+
+# --------------------------------------------------------
+
+# WAP to input the  side of a square and print its area
+
+# square_side = float(input("Enter the side of the square: "))
+# area_of_square = square_side * square_side
+# print("Area of the square: ", area_of_square)
+
+# -----------------------------------------------------------
+
+# WAP to input two floating point number and print their average
+
+# num_1 = float(input("Enter you num_1 : "))
+# num_2 = float(input("Enter your num_2 :"))
+# sum = num_1 + num_2
+# avg = sum/2
+# print("Average : ", avg)
+
+# -----------------------------------------------------------
+
