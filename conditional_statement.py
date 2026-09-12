cb92d209e0d0257ff49fceca083351206a3abd42
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-13 02:04:34 +0530

    learn elif condition

--- conditional_statement.py
+++ conditional_statement.py
@@ -29,9 +29,23 @@
 
 # WAP to input 2 int number A and B print True if A is greater than or equal to b if not print Falls
 
-a = int(input("Enter num_1: "))
-b = int(input("Enter num_2: "))
-if(a >= b):
-    print("True")
+# a = int(input("Enter num_1: "))
+# b = int(input("Enter num_2: "))
+# if(a >= b):
+#     print("True")
+# else:
+#     print("False")
+
+# ------------------------------------------------------
+
+# WAP to find grade students based on marks
+
+marks = float(input("Enter you marks : "))
+if (marks >= 90):
+    print("Your grade is A")
+elif (marks >= 80):
+    print( "Your grade is B")
+elif (marks >= 70):
+    print("Your grade is C")
 else:
-    print("False")
+    print("Your grade is D")
