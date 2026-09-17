1486357ab8296bd1bc49e72583d6bfa311fe0540
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-18 02:13:46 +0530

    practice of while loop

--- loops.py
+++ loops.py
@@ -1,28 +1,72 @@
 # basics of while loop
 
-count = 1
-while count <= 5:
-    print("Hello")
-    count += 1
-print(count)
+# count = 1
+# while count <= 5:
+#     print("Hello")
+#     count += 1
+# print(count)
 
-i = 1
-while i <= 5:
-    print("Abhishek", i)
-    i += 1
+# i = 1
+# while i <= 5:
+#     print("Abhishek", i)
+#     i += 1
 
-# print 1 to 5 
+# # print 1 to 5 
 
-no = 1 
-while no <= 5:
-    print(no)
-    no += 1
-print("loop ended")
+# no = 1 
+# while no <= 5:
+#     print(no)
+#     no += 1
+# print("loop ended")
 
-# print form 5 to 1 
+# # print form 5 to 1 
 
-num = 5
-while num >= 1:
-    print(num)
-    num -= 1
-print("loop ended")
+# num = 5
+# while num >= 1:
+#     print(num)
+#     num -= 1
+# print("loop ended")
+
+# -----------------------------
+
+# Print numbers from 1 to 100
+
+# nums = 1
+# while nums <= 100:
+#     print(nums)
+#     nums += 1
+    
+# -----------------------------
+
+# Print numbers from 100 to 1
+
+# numb = 100
+# while numb >= 1:
+#     print(numb)
+#     numb -= 1
+
+# ------------------------------------
+
+# Print the multiplication table of a number n
+
+# table = int(input("Enter your no. : "))
+# multiple = 1
+# while multi <= 10:
+#     print(table*multiple)
+#     multi += 1
+
+# --------------------------------------------
+
+# Print the elements of the following list using a loop
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+sq = 1
+while sq <= 10:
+    print(sq*sq)
+    sq += 1
+
+# ---------------------------------------------
+
+# Search for a number x in this tuple using loop
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
