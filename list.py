ec0e6da2bf645b000fe53ce19296289fd2eca2a1
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-17 12:32:05 +0530

    leran reverse() and copy()

--- list.py
+++ list.py
@@ -1,10 +1,24 @@
-# Write a program to ask the user to enter name of their three favorite movies and store them in a list
+# WAP to ask the user to enter name of their three favorite movies and store them in a list
 
-movie = []
-mov_1 = input("Enter your 1st fav movie name: ")
-mov_2 = input("Enter your 2nd fav movie name: ")
-mov_3 = input("Enter your 3rd fav movie name: ")
-movie.append(mov_1)
-movie.append(mov_2)
-movie.append(mov_3)
-print(movie)
+# movie = []
+# mov_1 = input("Enter your 1st fav movie name: ")
+# mov_2 = input("Enter your 2nd fav movie name: ")
+# mov_3 = input("Enter your 3rd fav movie name: ")
+# movie.append(mov_1)
+# movie.append(mov_2)
+# movie.append(mov_3)
+# print(movie)
+
+# ---------------------------------------------------------------
+
+# WAP to check if a list contain a palindromof elements or not [hint use copy() method]
+
+# # list = [1, 2, 3, 2, 1]
+
+# list = [1, 2, 1, 3]
+# copy_list = list.copy()
+# copy_list.reverse()
+# if (copy_list == list):
+#     print("Palindrone")
+# else:
+#     print("Not palindrone")
