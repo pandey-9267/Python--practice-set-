63c10f39ad27d3166c0f6d5e0e3dec241e5c2e75
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-17 12:23:43 +0530

    learn list append()

---
+++ list.py
@@ -0,0 +1,10 @@
+# Write a program to ask the user to enter name of their three favorite movies and store them in a list
+
+movie = []
+mov_1 = input("Enter your 1st fav movie name: ")
+mov_2 = input("Enter your 2nd fav movie name: ")
+mov_3 = input("Enter your 3rd fav movie name: ")
+movie.append(mov_1)
+movie.append(mov_2)
+movie.append(mov_3)
+print(movie)
