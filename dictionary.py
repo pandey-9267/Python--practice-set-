dc4d424cd10aeda84424b708705623873c3b3624
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-17 13:40:40 +0530

    learn accesing, replacing, how null dict work, add new key value in existing dict

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,26 @@
 dict = {
-    "name" : "abhishek",
+    "name" : "abhi",
     "cgpa" : 7.1,
     "marks" : [80, 98, 78]
 }
 print(dict)
 
+# accessong the key in dict
+
+print(dict["name"])
+
+# for repalcing the value 
+
+dict["name"] = "Abhishek"
+print(dict)
+
+# also add a new key vlaue 
+
+dict["surname"] = "pandey"
+print(dict)
+
+# how to add key value in a empty or null dict
+
+null_dict = {}
+null_dict["name"] = "Anush"
+print(null_dict)
