70d9233de40349ac59816bb7ddfbfe34826cfc57
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-17 14:40:58 +0530

    learn dict methods

--- dictionary.py
+++ dictionary.py
@@ -27,15 +27,76 @@
 
 # How nested dictionay work
 
-students = {
-    "Name" : "Rahul",
-    "Subjects" : {
-        "chem" : 88,
-        "Phy" : 98,
-        "Pyhton" : 100
-    }
-}
+# students = {
+#     "Name" : "Rahul",
+#     "Subjects" : {
+#         "chem" : 88,
+#         "Phy" : 98,
+#         "Pyhton" : 100
+#     }
+# }
 
+# print(students)
+# print(students["Subjects"])
+# print(students["Subjects"] ["Pyhton"])
+
+# METHODS
+students = {
+      "Name" : "Rahul",
+      "Subjects" : {
+          "chem" : 88,
+          "Phy" : 98,
+          "Pyhton" : 100
+      }
+  }
+
+# this return all the keys 
+
+print(students.keys())
+
+# this convert in to list or anything you want do like this 
+
+print(list(students))
+print(tuple(students))
+
+# this return all the values 
+
+print(students.values())
+
+# this return the length of the dict 
+
+print(len(students))
+
+# this return all the key value pairs in the tuple
+
+# print(students.items())
+
+# and if want to access the single tuple from the above 
+
+pairs = list(students.items())
+print(pairs[0])
+
+# retuen the kay according to the value
+# there are two ways 
+
+# print(students["Name"])  
+
+# this way give an    error  this is not not bcoz after error other lines of code which is correct that also not run 
+# print(students["name2"]) 
+
+
+print(students.get("Name"))
+
+# # this way did not give erroe is give   none but here the runs the all line 
+print(students.get("name2"))
+
+# update the dict 
+
+# students.update({"city" : "Delhi"})
+# print(students)
+
+# also do like this 
+
+new_dict = {"city" : "Delhi"}
+students.update(new_dict)
 print(students)
-print(students["Subjects"])
-print(students["Subjects"] ["Pyhton"])
