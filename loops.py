a4c29fddc0ff249520335858f12635a51d8149eb
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-18 01:26:08 +0530

    basics of while loop

---
+++ loops.py
@@ -0,0 +1,28 @@
+# basics of while loop
+
+count = 1
+while count <= 5:
+    print("Hello")
+    count += 1
+print(count)
+
+i = 1
+while i <= 5:
+    print("Abhishek", i)
+    i += 1
+
+# print 1 to 5 
+
+no = 1 
+while no <= 5:
+    print(no)
+    no += 1
+print("loop ended")
+
+# print form 5 to 1 
+
+num = 5
+while num >= 1:
+    print(num)
+    num -= 1
+print("loop ended")
