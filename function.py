32da1d0c989ebbd12ec753d6090c3159f031b32a
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-25 20:00:42 +0530

    learn basics of function

---
+++ function.py
@@ -0,0 +1,21 @@
+# Basics of function
+
+# def calc_sum(a, b):
+#     return a + b
+
+# print(calc_sum(1, 2))
+
+
+# def print_hello():
+#     print("Hello")
+
+# print_hello()
+# print_hello()
+
+def calc_avg(x, y, z):
+    sum = x + y + z
+    avg = sum/3
+    print(avg)
+    return avg
+
+calc_avg(1, 2, 3)
