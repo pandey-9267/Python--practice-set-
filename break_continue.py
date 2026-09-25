ba6d3c5513185809f794a631b58484cee840fe5e
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-25 11:08:34 +0530

    leran about break and continue

---
+++ break_continue.py
@@ -0,0 +1,28 @@
+# Break 
+# i = 0 
+# while i <= 5:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+# continue 
+
+# i = 0
+# while i <= 5:
+#     if(i == 2):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+
+# both do same thing 
+
+# i = 0
+# while i < 5:
+#     i += 1  # Increment first so we don't get stuck in an infinite loop!
+#     if i == 3:
+#         print("Skipping 3...")
+#         continue  # Jumps straight back to 'while i < 5', ignoring the print below
+#     print(f"Current number: {i}")
