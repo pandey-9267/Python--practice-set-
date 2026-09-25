fe3aa87856485ff9264cde27c255c4e843cdd69e
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-25 10:48:10 +0530

    learn how to use break in while loop

--- loops.py
+++ loops.py
@@ -60,13 +60,24 @@
 # Print the elements of the following list using a loop
 # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
-sq = 1
-while sq <= 10:
-    print(sq*sq)
-    sq += 1
+# sq = 1
+# while sq <= 10:
+#     print(sq*sq)
+#     sq += 1
 
 # ---------------------------------------------
 
 # Search for a number x in this tuple using loop
 # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+x = 36
+
+i = 0
+while i < len(nums):
+    if nums[i] == x:
+        print("Founded", i)
+        break
+    else: 
+      print("Finding...")
+    i += 1
